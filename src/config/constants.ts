506eb27e005a89e7dab99ab1b6c124be7c91d59c
Alexandre-Akira <akira.enjiu@gmail.com>
2022-09-07 16:07:08 -0300

    refactor: moved database and constants file to config folder

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-export const DB_PORT = process.env.DB_PORT
+// export const DB_PORT = process.env.DB_PORT
 export const DB_USER = process.env.DB_USER
 export const DB_NAME = process.env.DB_NAME
 export const DB_PASSWORD = process.env.DB_PASSWORD
--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,12 @@
 import { Options } from 'sequelize/types'
+import { DB_NAME, DB_PASSWORD, DB_USER } from './constants'
 
 export const dbConfig: Options = {
   dialect: 'postgres',
   host: 'localhost',
-  username: 'colocar o nome do usuario',
-  password: 'password do banco',
-  database: 'nome da database',
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   define: {
     timestamps: true, // gera o campo createdAt e updatedAt que contem data e hora
     underscored: true // define o nome das tabelas e nome das colunas como snake case
