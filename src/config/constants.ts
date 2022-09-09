4d040d19aeec21a4e4260768f9ae26203dfbf1b3
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-09-09 14:16:47 -0300

    chore: add database host to dotenv

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 export const PORT = process.env.PORT
 
 // DB Constants
+export const DB_HOST = process.env.DB_HOST
 export const DB_USER = process.env.DB_USER
 export const DB_NAME = process.env.DB_NAME
 export const DB_PASSWORD = process.env.DB_PASSWORD
