1ad9412639479b6262e9b73e5530b83910d335eb
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-09-10 18:05:46 -0300

    feat: add import export for allControllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+import UserController from './userController'
+import TodoController from './todoController'
+
+const allControllers = [UserController, TodoController]
+
+export default allControllers
