bf3667eb5f2510425c722c8ca90ffa180dbf29be
Guilherme Pereira de Oliveira <gui88po@gmail.com>
2022-09-11 15:10:37 -0300

    feat: add create_user_table migration file

---
+++ src/database/migrations/20220911145507-create_user_table.js
@@ -0,0 +1,42 @@
+'use strict'
+import { DataTypes } from 'sequelize/types'
+
+export async function up(queryInterface, Sequelize) {
+  /**
+   * Add altering commands here.
+   *
+   * Example:
+   * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+   */
+  await queryInterface.createTable('users', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  })
+}
+export async function down(queryInterface, Sequelize) {
+  /**
+   * Add reverting commands here.
+   *
+   * Example:
+   * await queryInterface.dropTable('users');
+   */
+  await queryInterface.dropTable('users')
+}
