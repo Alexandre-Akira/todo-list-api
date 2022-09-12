3cacbe1427151c0756b006d5d58819a1bb242ff5
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-09-12 20:40:12 -0300

    feat: add authentication middleware to routes

--- src/routes/todoRoutes/index.ts
+++ src/routes/todoRoutes/index.ts
@@ -1,16 +1,33 @@
 import { Router } from 'express'
 
 import { TodoController } from '../../controllers'
+import authenticationMiddleware from '../../middlewares/authentication'
 
 const todoRoutes = Router()
 const BASE_PATH = '/todo'
 
-todoRoutes.post(`${BASE_PATH}`, TodoController.createTodo)
+todoRoutes.post(
+  `${BASE_PATH}`,
+  authenticationMiddleware,
+  TodoController.createTodo
+)
 
-todoRoutes.get(`${BASE_PATH}/:id?`, TodoController.getTodos)
+todoRoutes.get(
+  `${BASE_PATH}/:id?`,
+  authenticationMiddleware,
+  TodoController.getTodos
+)
 
-todoRoutes.put(`${BASE_PATH}/:id`, TodoController.updateTodo)
+todoRoutes.put(
+  `${BASE_PATH}/:id`,
+  authenticationMiddleware,
+  TodoController.updateTodo
+)
 
-todoRoutes.delete(`${BASE_PATH}/:id`, TodoController.deleteTodo)
+todoRoutes.delete(
+  `${BASE_PATH}/:id`,
+  authenticationMiddleware,
+  TodoController.deleteTodo
+)
 
 export default todoRoutes
--- src/routes/userRoutes/index.ts
+++ src/routes/userRoutes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { UserController } from '../../controllers'
+import authenticationMiddleware from '../../middlewares/authentication'
 
 const userRoutes = Router()
 const BASE_PATH = '/user'
@@ -8,10 +9,22 @@ const BASE_PATH = '/user'
 userRoutes.post(`${BASE_PATH}`, UserController.createUser)
 userRoutes.post(`${BASE_PATH}/login`, UserController.login)
 
-userRoutes.get(`${BASE_PATH}/:id`, UserController.getUser)
+userRoutes.get(
+  `${BASE_PATH}/:id`,
+  authenticationMiddleware,
+  UserController.getUser
+)
 
-userRoutes.put(`${BASE_PATH}/:id`, UserController.updateUser)
+userRoutes.put(
+  `${BASE_PATH}/:id`,
+  authenticationMiddleware,
+  UserController.updateUser
+)
 
-userRoutes.delete(`${BASE_PATH}/:id`, UserController.deleteUser)
+userRoutes.delete(
+  `${BASE_PATH}/:id`,
+  authenticationMiddleware,
+  UserController.deleteUser
+)
 
 export default userRoutes
