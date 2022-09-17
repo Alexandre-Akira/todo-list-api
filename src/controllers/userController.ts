639d61eca65bc27f330149705621deabed80354c
Alexandre-Akira <akira.enjiu@gmail.com>
2022-09-17 19:36:06 -0300

    refactor: implemented userModel in userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,21 +4,8 @@ import jwt from 'jsonwebtoken'
 
 const SECRET_KEY = require('../database/constants.ts')
 
-// Verificar os nomes dos metódos dos models quando estiver pronto
-import {
-  createNewUser,
-  getUserByEmail,
-  getUserById,
-  updateUserById,
-  deleteUserById,
-  UserModel  
-} from '../models/UserModel'
-import { password } from 'pg/lib/defaults'
-
-// Verificar os nomes dos DTOs das views quando estiver pronto
-// import UserDTO from '../views/userDTO'
-// import TokenDTO from '../views/tokenDTO'
-
+import UserModel from '../models/userModel'
+import { isUUID } from '../utils'
 
 class UserController {
   static async createUser(req: Express.Request, res: Express.Response) {
@@ -31,13 +18,14 @@ class UserController {
     const salt = await bcrypt.genSalt(12)
     const passwordHash = await bcrypt.hash(password, salt)
 
-    const newUser = await UserModel.createNewUser(name, email, passwordHash)
+    const [newUser, isCreated] = await UserModel.user.findOrCreate({
+      where: { email },
+      defaults: { name, email, password: passwordHash }
+    })
+    // TODO: if (isCreated) return res.status(201).json(new UserDTO(newUser))
+    if (isCreated) return res.status(201).json(newUser)
 
-    // if (newUser.userAlreadyRegistered) {
-    //   return res.status(409).send('E-mail already registred')
-    // }
-
-    return res.status(201).json(newUser)
+    return res.status(409).send('E-mail already registred')
   }
 
   static async login(req: Express.Request, res: Express.Response) {
@@ -47,7 +35,7 @@ class UserController {
       return res.status(400).send('Missing data')
     }
 
-    const user = await getUserByEmail(email)
+    const user = await UserModel.user.findOne({ where: { email } })
 
     if (!user) {
       return res.status(403).send('Unauthorized')
@@ -59,11 +47,12 @@ class UserController {
       return res.status(403).send('Unauthorized')
     }
 
-    const jwtToken = jwt.sign({ UserId: user.id }, SECRET_KEY as string, {
+    const jwtToken = jwt.sign({ UserId: user.id }, SECRET_KEY.toString(), {
       expiresIn: '1h'
     })
 
-    return res.status(201).json(new TokenDTO(jwtToken))
+    // TODO: return res.status(201).json(new TokenDTO(jwtToken))
+    return res.status(201).json(jwtToken)
   }
 
   static async getUser(req: Express.Request, res: Express.Response) {
@@ -73,13 +62,16 @@ class UserController {
       return res.status(400).send('Missing data')
     }
 
-    const user = await getUserById(id)
+    if (!isUUID(id)) return res.status(422).send('Invalid ID')
+
+    const user = await UserModel.user.findOne({ where: { id } })
 
     if (!user) {
       return res.status(404).send('User not found')
     }
 
-    return res.status(200).json(new UserDTO(user))
+    // return res.status(200).json(new UserDTO(user))
+    return res.status(200).json(user)
   }
 
   static async updateUser(req: Express.Request, res: Express.Response) {
@@ -89,7 +81,11 @@ class UserController {
       return res.status(400).send('Missing data')
     }
 
-    const user = await getUserById(id)
+    if (!isUUID(id)) {
+      return res.status(422).send('Invalid ID')
+    }
+
+    const user = await UserModel.user.findOne({ where: { id } })
 
     if (!user) {
       return res.status(404).send('User not found')
@@ -104,9 +100,13 @@ class UserController {
     const salt = await bcrypt.genSalt(12)
     const passwordHash = await bcrypt.hash(password, salt)
 
-    const updatedUser = await updateUserById({ name, email, passwordHash })
+    const updatedUser = await UserModel.user.update(
+      { name, email, passwordHash },
+      { where: { id } }
+    )
 
-    return res.status(200).json(new UserDTO(updatedUser))
+    // return res.status(200).json(new UserDTO(updatedUser))
+    return res.status(200).json(updatedUser)
   }
 
   static async deleteUser(req: Express.Request, res: Express.Response) {
@@ -116,7 +116,11 @@ class UserController {
       return res.status(400).send('Missing data')
     }
 
-    await deleteUserById(id)
+    if (!isUUID(id)) {
+      return res.status(422).send('Invalid ID')
+    }
+
+    await UserModel.user.destroy({ where: { id } })
 
     return res.status(204).send()
   }
