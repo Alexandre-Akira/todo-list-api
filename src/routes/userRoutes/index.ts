d88cf1724fd82297188694886fe7b3d25706aaa5
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2022-09-18 22:57:22 -0300

    refactor: removed optional params on user routes

--- src/routes/userRoutes/index.ts
+++ src/routes/userRoutes/index.ts
@@ -8,10 +8,10 @@ const BASE_PATH = '/user'
 userRoutes.post(`${BASE_PATH}`, UserController.createUser)
 userRoutes.post(`${BASE_PATH}/login`, UserController.login)
 
-userRoutes.get(`${BASE_PATH}/:id?`, UserController.getUser)
+userRoutes.get(`${BASE_PATH}/:id`, UserController.getUser)
 
-userRoutes.put(`${BASE_PATH}/:id?`, UserController.updateUser)
+userRoutes.put(`${BASE_PATH}/:id`, UserController.updateUser)
 
-userRoutes.delete(`${BASE_PATH}/:id?`, UserController.deleteUser)
+userRoutes.delete(`${BASE_PATH}/:id`, UserController.deleteUser)
 
 export default userRoutes
